45b2ddeba69f4a7982794ed1dfbec3d605c7dfe2
theGeekkit <sonofgondolin16@gmail.com>
2023-07-04 16:35:42 -0500

    refactored seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,70 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Adjuster.find_or_create_by(name: 'Pickles') do |adjuster|
-    adjuster.price = 0.00
+adjusters = [
+  { name: 'Pickles', price: 0.00 },
+  { name: 'Ketchup', price: 0.00 },
+  { name: 'Mustard', price: 0.00 },
+  { name: 'Onions', price: 0.00 },
+  { name: 'Mayo', price: 0.00 },
+  { name: 'Lettuce', price: 0.00 },
+  { name: 'Tomato', price: 0.40 },
+  { name: 'Bacon', price: 0.80 },
+  { name: 'Mushrooms', price: 0.60 },
+  { name: 'Grilled Onions', price: 0.10 },
+  { name: 'Fries', price: 0.20 },
+  { name: 'BBQ Sauce', price: 0.00 },
+  { name: 'Hot Sauce', price: 0.00 },
+  { name: 'Well Done', price: 0.00 },
+  { name: 'No Seasoning', price: 0.00 },
+  { name: 'No Bun', price: 0.00 },
+  { name: 'Gluten Free Bun', price: 2.00 }
+]
+
+adjusters.each do |adjuster|
+  Adjuster.find_or_create_by(name: adjuster[:name]) do |a|
+    a.price = adjuster[:price]
   end
-  
-  Adjuster.find_or_create_by(name: 'Ketchup') do |adjuster|
-    adjuster.price = 0.00
-  end
-  
-  Adjuster.find_or_create_by(name: 'Mustard') do |adjuster|
-    adjuster.price = 0.00
-  end
-  
-  Adjuster.find_or_create_by(name: 'Onions') do |adjuster|
-    adjuster.price = 0.00
-  end
-  
-  Adjuster.find_or_create_by(name: 'Mayo') do |adjuster|
-    adjuster.price = 0.00
-  end
-  
-  Adjuster.find_or_create_by(name: 'Lettuce') do |adjuster|
-    adjuster.price = 0.00
-  end
-  
-  Adjuster.find_or_create_by(name: 'Tomato') do |adjuster|
-    adjuster.price = 0.40
-  end
-  
-  Adjuster.find_or_create_by(name: 'Bacon') do |adjuster|
-    adjuster.price = 0.80
-  end
-  
-  Adjuster.find_or_create_by(name: 'Mushrooms') do |adjuster|
-    adjuster.price = 0.60
-  end
-  
-  Adjuster.find_or_create_by(name: 'Grilled Onions') do |adjuster|
-    adjuster.price = 0.10
-  end
-  
-  Adjuster.find_or_create_by(name: 'Fries') do |adjuster|
-    adjuster.price = 0.20
-  end
-  
-  Adjuster.find_or_create_by(name: 'BBQ Sauce') do |adjuster|
-    adjuster.price = 0.00
-  end
-  
-  Adjuster.find_or_create_by(name: 'Hot Sauce') do |adjuster|
-    adjuster.price = 0.00
-  end
-  
-  Adjuster.find_or_create_by(name: 'Well Done') do |adjuster|
-    adjuster.price = 0.00
-  end
-  
-  Adjuster.find_or_create_by(name: 'No Seasoning') do |adjuster|
-    adjuster.price = 0.00
-  end
-  
-  Adjuster.find_or_create_by(name: 'No Bun') do |adjuster|
-    adjuster.price = 0.00
-  end
-  
-  Adjuster.find_or_create_by(name: 'Gluten Free Bun') do |adjuster|
-    adjuster.price = 2.00
-  end
+end

82ffabf03f6d8e1004afe6ace470d6d4ee924b8b
theGeekkit <sonofgondolin16@gmail.com>
2023-07-10 18:23:11 -0500

    7-10-23

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-# adjusters = [
-#   { name: 'Pickles', price: 0.00 },
-#   { name: 'Ketchup', price: 0.00 },
-#   { name: 'Mustard', price: 0.00 },
-#   { name: 'Onions', price: 0.00 },
-#   { name: 'Mayo', price: 0.00 },
-#   { name: 'Lettuce', price: 0.00 },
-#   { name: 'Tomato', price: 0.40 },
-#   { name: 'Bacon', price: 0.80 },
-#   { name: 'Mushrooms', price: 0.60 },
-#   { name: 'Grilled Onions', price: 0.10 },
-#   { name: 'Fries', price: 0.20 },
-#   { name: 'BBQ Sauce', price: 0.00 },
-#   { name: 'Hot Sauce', price: 0.00 },
-#   { name: 'Well Done', price: 0.00 },
-#   { name: 'No Seasoning', price: 0.00 },
-#   { name: 'No Bun', price: 0.00 },
-#   { name: 'Gluten Free Bun', price: 2.00 }
-# ]
 
-# adjusters.each do |adjuster|
-#   Adjuster.find_or_create_by(name: adjuster[:name]) do |a|
-#     a.price = adjuster[:price]
-#   end
 # end
